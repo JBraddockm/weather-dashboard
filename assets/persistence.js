726a6cdeaae5e3c92e565c3089a33e78c0d5f9f9
JBraddockm <jbraddockm@gmail.com>
2024-01-30 10:30:12 +0000

    Created a function to save an object to localStorage with the given key.

--- assets/persistence.js
+++ assets/persistence.js
@@ -2,7 +2,7 @@ let cities = new Map();
 
 // Function to retrieve entities items from localStorage.
 function loadEntityFromLocalStorage(key) {
-    // Retrieve the stored data from localStorage
+    // Retrieves the stored data from localStorage
     const storedData = JSON.parse(localStorage.getItem(key));
 
     // If there's no stored data, return an empty Map
@@ -10,6 +10,18 @@ function loadEntityFromLocalStorage(key) {
         return new Map();
     }
 
-    // Convert the stored data back into a Map
+    // Converts the stored data back into a Map
     return new Map(storedData);
+}
+
+// Function to save entities to localStorage.
+function saveEntityToLocalStorage(entity, key) {
+    // Retrieves existing entities from localStorage.
+    const collectionList = loadEntityFromLocalStorage(key);
+
+    // Adds the new entity to the map. Lower
+    collectionList.set(entity.id.toLowerCase(), entity);
+
+    // Stores the updated map back in localStorage.
+    localStorage.setItem(key, JSON.stringify([...collectionList]));
 }
