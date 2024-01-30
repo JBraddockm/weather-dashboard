5377525dd12a60f09435621adafeb015cfe1e035
JBraddockm <jbraddockm@gmail.com>
2024-01-30 10:39:44 +0000

    Added variables for handling location search.

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,8 @@
 
 const APIKey = 'b1d2eae6a0a395f178e0b75365981fcc';
 const baseURL = 'https://api.openweathermap.org';
+
+const searchHistory = new Set();
+const searchHistoryEl = $('#history');
+const searchFormEl = $('#search-form');
+let cityName;
